028a30d9f77fc5528ec77bca35c94f1f6089d937
Alice-anjali <alice.anjali1995@gmail.com>
2018-06-07 18:29:35 +0530

    alternate method for peak 2D

--- MIT/2.peak_finder-2D.py
+++ MIT/2.peak_finder-2D.py
@@ -6,20 +6,16 @@ for i in range(n):
 
 def find_peak(matrix,n,m):
     start = 0
-    end = m-1
+    end = n-1
     peak = [-9,-9]
     while(start <= end):
         mid = (start+end)//2
-        maxInColm = matrix[0][mid]
-        maxRow = 0
-        for i in range(n):
-            if maxInColm < matrix[i][mid]:
-                maxInColm = matrix[i][mid]
-                maxRow = i
-        if((mid == 0 or matrix[maxRow][mid] > matrix[maxRow][mid-1])and(mid == m-1 or matrix[maxRow][mid] > matrix[maxRow][mid+1])):
-            peak = [maxRow,mid]
+        maxInRow = max(matrix[mid])
+        maxColmIndex = matrix[mid].index(maxInRow)
+        if((mid == 0 or matrix[mid][maxColmIndex] > matrix[mid-1][maxColmIndex])and(mid == n-1 or matrix[mid][maxColmIndex] > matrix[mid+1][maxColmIndex])):
+            peak = [mid, maxColmIndex]
             return peak
-        elif matrix[maxRow][mid] < matrix[maxRow][mid-1]:
+        elif matrix[mid][maxColmIndex] < matrix[mid-1][maxColmIndex]:
             end = mid-1
         else:
             start = mid+1
@@ -32,3 +28,42 @@ if peak is not -999:
     print(matrix[peak[0]][peak[1]])
 else:
     print("Peak not found")
+
+
+
+#USING ITERATIION, row column altered here
+
+# n, m = map(int, input("Enter rows and columns: ").strip().split(' '))
+# matrix = []
+# for i in range(n):
+#     row = list(map(int, input().strip().split(' ')))
+#     matrix.append(row)
+#
+# def find_peak(matrix,n,m):
+#     start = 0
+#     end = m-1
+#     peak = [-9,-9]
+#     while(start <= end):
+#         mid = (start+end)//2
+#         maxInColm = matrix[0][mid]
+#         maxRow = 0
+#         for i in range(n):
+#             if maxInColm < matrix[i][mid]:
+#                 maxInColm = matrix[i][mid]
+#                 maxRow = i
+#         if((mid == 0 or matrix[maxRow][mid] > matrix[maxRow][mid-1])and(mid == m-1 or matrix[maxRow][mid] > matrix[maxRow][mid+1])):
+#             peak = [maxRow,mid]
+#             return peak
+#         elif matrix[maxRow][mid] < matrix[maxRow][mid-1]:
+#             end = mid-1
+#         else:
+#             start = mid+1
+#
+#     return -999
+#
+# peak = find_peak(matrix,n,m)
+# if peak is not -999:
+#     print(peak)
+#     print(matrix[peak[0]][peak[1]])
+# else:
+#     print("Peak not found")
